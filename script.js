30351a6894bda742ebdfb63f3900e03214ab096a
G0nZy <knicksfan1297@gmail.com>
2020-06-05 19:50:55 -0400

    new things

--- script.js
+++ script.js
@@ -201,7 +201,16 @@ getResults(searchbox.value);
             let hilow = document.querySelector(".hi-low");
                        hilow.innerText= Hi/Lo - `${weather.main.temp_min}°c / ${weather.main.temp_max}°c`;
                                    
-                                    }
+                                    
+                    
+
+
+                      
+
+                    
+                    
+                    
+                    }
 
             function currentDate (d){
 
@@ -217,7 +226,26 @@ getResults(searchbox.value);
                    return `${day} ${date} ${month} ${year} `;
                                         }
 
-            
+    
+
+   
+                                        forecast.forEach(day => {
+                                            let date = new Date(day.dt * 1000);
+                                            let days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+                                            let name = days[date.getDay()];
+                                            let dayBlock = document.createElement("div");
+                                            dayBlock.className = 'forecast__item';
+                                            dayBlock.innerHTML =
+                                              `<div class="forecast-item__heading">${name}</div>
+                                              <div class="forecast-item__info">
+                                              <i class="wi ${applyIcon(day.weather[0].icon)}"></i>
+                                              <span class="degrees">${Math.round(day.temp.day)}
+                                              <i class="wi wi-degrees"></i></span></div>`;
+                                            FORECAST.appendChild(dayBlock);
+                                          });
+                                        
+
+                        
 
 
 
